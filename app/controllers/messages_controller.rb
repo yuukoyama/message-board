f0e05fddc59c92c24fdaf108b3f2180bc96d7d34
yuukoyama <kym1028kym@gmail.com>
2017-02-03 12:20:34 +0000

    delete-fuction

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,13 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
+
+  # 中略
   def index
     @message = Message.new
     # Messageを全て取得する。
     @messages = Message.all
   end
-
+  
   def create
     @message = Message.new(message_params)
     if @message.save
@@ -16,11 +19,32 @@ class MessagesController < ApplicationController
       render 'index'
     end
   end
+    
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
+
   private
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
+  
   def message_params
     params.require(:message).permit(:name, :body)
   end
-  ## ここまで
 end
-
-
